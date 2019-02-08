36a992b26886961022a863c6eecadd1b63f04543
shrouktarek98 <shrouk.talat98@gmail.com>
2019-02-08 03:51:10 +0200

    vf

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FotterComponent } from './fotter/fotter.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FotterComponent
   ],
   imports: [
     BrowserModule,
